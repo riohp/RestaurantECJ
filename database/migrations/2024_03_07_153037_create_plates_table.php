d0f5454ee3a34e427fc20a460a56e23df59dd48b
Luna717 <lunarave911@gmail.com>
2024-03-07 11:36:58 -0500

    first commit

---
+++ database/migrations/2024_03_07_153037_create_plates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->float('price',10,2);
+            $table->float('cost',10,2);
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
